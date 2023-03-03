e5daca0f0eea58ce18c9c1dcbdf6ebe44fc2ee50
Luke D <luke.warmwater6@gmail.com>
2023-03-04 12:59:02 +1300

    Reduce image max width

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 1200,
+              maxWidth: 900,
             },
           },
         ],
