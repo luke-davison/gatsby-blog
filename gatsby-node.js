86c680cb79c3ec52d79b0b4384c6f8a7163f1316
Luke D <luke.warmwater6@gmail.com>
2023-03-11 11:04:25 +1300

    Reverted reversion

--- gatsby-node.js
+++ gatsby-node.js
@@ -46,6 +46,7 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
             fields {
               slug
               written
+              category
             }
             internal {
               contentFilePath
@@ -62,41 +63,45 @@ exports.createPages = async ({ graphql, actions, reporter }) => {
 
   // Create blog post pages.
   const posts = Array.from(result.data.allMdx.edges)
-  posts.sort(
-    (postA, postB) =>
-      new Date(postB.node.fields.written).getTime() -
-      new Date(postA.node.fields.written).getTime()
-  )
 
   const template = path.resolve(`./src/components/post-page-template.js`)
-
-  posts.forEach(({ node }, index) => {
-    const previousId = posts.find(
-      (previous, previousIndex) => previousIndex === index + 1
-    )?.node.id
-    const nextId = posts.find((next, nextIndex) => nextIndex === index - 1)
-      ?.node.id
-
-    createPage({
-      path: node.fields.slug,
-      component: `${template}?__contentFilePath=${node.internal.contentFilePath}`,
-      context: { id: node.id, previousId, nextId },
-    })
-  })
 
   const categoryIds = [
     "north-america-2023",
     "south-island-2022",
     "new-zealand-2020",
     "north-america-2019",
-    "miscellaneous"
+    "miscellaneous",
   ]
 
-  categoryIds.forEach(category => {
+  categoryIds.forEach((category) => {
     createPage({
       path: "/" + category,
       component: path.resolve(`./src/components/category-page-template.js`),
       context: { id: category },
+    })
+
+    const categoryPosts = posts.filter(
+      post => post.node.fields.category === category
+    )
+
+    categoryPosts.sort(
+      (postA, postB) =>
+        new Date(postB.node.fields.written).getTime() -
+        new Date(postA.node.fields.written).getTime()
+    )
+
+    categoryPosts.forEach(({ node }, index) => {
+      const previousId = categoryPosts.find(
+        (previous, previousIndex) => previousIndex === index + 1
+      )?.node.id
+      const nextId = categoryPosts.find((next, nextIndex) => nextIndex === index - 1)
+        ?.node.id
+      createPage({
+        path: node.fields.slug,
+        component: `${template}?__contentFilePath=${node.internal.contentFilePath}`,
+        context: { id: node.id, previousId, nextId },
+      })
     })
   })
 }
